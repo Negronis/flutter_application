581805cfb4c00da70980aaba971c1034cfc060d0
Negronis <57703160+Negronis@users.noreply.github.com>
2025-01-24 21:01:01 +0800

    更新 home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:my_application/util/dialog.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+    var dialog = MyDialog(context: context);
+    initState() {
+      super.initState();
+      dialog.showLoading();
+    }
+
     return Scaffold(
       body: Center(
         child: Text("我是首页"),
